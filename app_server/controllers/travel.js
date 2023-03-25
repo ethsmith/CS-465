4b8ee5b7f52ba3fe0573324dfea7dad88f7c8b58
ethsmith <ethan.smith1845@gmail.com>
2023-03-25 18:42:34 -0500

    Module 3

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,9 @@
+var fs = require("fs");
+
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf-8"));
+
 const travel = (req, res) => {
-  res.render("travel", { title: "Travlr Getaways" });
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 
 module.exports = {
