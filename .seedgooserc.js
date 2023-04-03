e1c53c4d34b9013cf81a4f8933f162848a974ea4
ethsmith <ethan.smith1845@gmail.com>
2023-04-02 20:01:59 -0500

    Module 4

---
+++ .seedgooserc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  modelBaseDirectory: "app_server/models", // model directory name
+  models: "**/*.js", // model matcher
+  data: "data", // data directory name
+  db: "mongodb://127.0.0.1/travlr", // db connection url
+};
---
+++ app_server/models/db.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const host = process.env.DB_HOST || "127.0.0.1";
+const dbURI = `mongodb://${host}/travlr`;
+const readLine = require("readline");
+
+const connect = () => {
+  setTimeout(
+    () =>
+      mongoose.connect("mongodb://127.0.0.1/travlr", {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      }),
+    1000
+  );
+};
+
+mongoose.connection.on("connected", () => {});
+
+mongoose.connection.on("error", (err) => {});
+
+mongoose.connection.on("disconnected", () => {});
+
+if (process.platform === "win32") {
+}
+
+const gracefulShutdown = (msg, callback) => {};
+
+process.once("SIGUSR2", () => {});
+
+process.on("SIGINT", () => {});
+
+process.on("SIGTERM", () => {});
+
+connect();
+
+require("./travlr");
